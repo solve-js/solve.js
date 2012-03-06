be8878cd5bc124a6b8731764dbc8b98cc0c603b8
Kyle Marcey <marcey.kyle@gmail.com>
2012-03-05 21:57:00 -0500

    Mostly complete implementation of an integrator using Euler's method for numerically solving differential equations

---
+++ src/solver.js
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2012, Kyle Marcey, Gregory Lee
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+var solver = (function () {
+    var s = {};
+
+    /**
+     * @function
+     * @param {Function} deFunction The function representing the Differential Equation to be solved
+     * @param {Number[]} y0 An array containing the initial values to the solution. Must be provided as an array, even if there is only one value.
+     * @param {Number} t0 The time value at which to begin integrating the function.
+     * @param {Number} tf The end time value where the integration is stopped.
+     * @param {Number} stepSize The width used for calculating each step in the integration.
+     * @param {Function} [integrator] Optional function that will be used for calculating steps in the integration. If a function is not supplied, the library will use the default method (Euler's).
+     *
+     * @returns {Object} Returns an object with two properties: t is an array filled with the time values at which the DE was solved;
+     * y is an n-dimensional array where each dimension is an array of values with length 't' containing the soluton to the differential equation.
+     */
+    s.solve = function (deFunction, y0, t0, tf, stepSize, integrator) {
+        "use strict"
+        try {
+            Verify.value(deFunction, "deFunction").always().isFunction();
+            Verify.value(y0, "y0").always().isArray().ofFiniteNumbers();
+            Verify.value(t0, "t0").always().isNumber().isFinite();
+            Verify.value(tf, "tf").always().isNumber().isFinite().notEqualTo(t0);
+            Verify.value(stepSize, "stepSize").always().isNumber().lessThan(Math.abs(tf - t0));
+            Verify.value(integrator, "integrator").whenDefined().isFunction();
+
+            var results = {y: [], t: []};
+            var stepper = integrator || s.eulerStep;
+            var ndims = y0.length;
+            var soln = [];
+            var timevals = [];
+
+            for (var dim = 0; dim < ndims; dim++) {
+                soln.push([y0[dim]]);
+            }
+            timevals.push(t0);
+
+            var currentValue = y0;
+            var dt = stepSize;
+
+            for (var t = t0; t < tf; t += dt) {
+                var step = stepper(deFunction, t, currentValue, dt);
+                currentValue = step;
+
+                for (var dim = 0; dim < ndims; dim++) {
+                    soln[dim].push([currentValue[dim]]);
+                }
+                timevals.push(t+dt);
+            }
+            results.y = soln;
+            results.t = timevals;
+            return results;
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    };
+
+
+    /**
+     * @description
+     * <p>
+     *     Generates an array of time points starting at 't0' and ending at 'tf', with points separated by 'increment'.
+     *     If the time values are increasing (t0 < tf), the increment must be positive. If the time values are decreasing (t0 > tf), the increment must be negative.
+     * </p>
+     *
+     * @param {Number} t0 The starting value for your time vector
+     * @param {Number} tf The end value for your time vector
+     * @param {Number} increment The size of the increment between values in the vector.
+     * Note: if your t0 value is *greater* than your tf value (i.e. you're going backwards in time), the value of the increment *must* be negative
+     *
+     */
+    s.createTimeVector = function (t0, tf, increment) {
+        "use strict"
+        var t = [];
+        var inc = increment;
+        var end = tf;
+        var start = t0;
+        var dError;
+
+        Verify.value(start, "start").always().isNumber().isFinite();
+        Verify.value(end, "end").always().isNumber().isFinite().notEqualTo(start);
+        Verify.value(inc, "increment").always().isNumber().isFinite().lessThan(Math.abs(start-end));
+
+        if(start < end){
+            Verify.value(inc, "increment").always().isNumber().greaterThan(0);
+        } else {
+            Verify.value(inc, "increment").always().isNumber().lessThan(0);
+        }
+            var lhs = [];
+            var rhs = [];
+            var numpoints = Math.ceil((end-start)/inc);
+            var midpoint = Math.floor(numpoints/2);
+            var i;
+            var offset;
+            for(i = 0; i < midpoint; i++){
+                offset = inc*i;
+                lhs.push(start + offset);
+                rhs.unshift(end - offset);
+            }
+            if(numpoints % 2 === 0){
+                lhs.push((start+end)/2);
+            } else {
+                offset = inc * midpoint;
+                lhs.push(start + offset);
+                rhs.unshift(end - offset);
+            }
+
+            t = lhs.concat(rhs);
+            return t;
+    };
+
+    /**
+     * @function
+     * @param {Function} ydot The function representing the Differential Equation that is being solved
+     * @param {Number} t The time value at which the DE is being evaluated for this step
+     * @param {Number[]} y An array containing the previous value(s) for the integrated DE. Should always be in an array, even if there is only one value.
+     * @param {Number} dt The size of the step to be used in calculating this next step of the integration.
+     *
+     * @returns {Number[]} Array containing the next step for each dimension of the provided DE
+     */
+    s.eulerStep = function (ydot, t, y, dt) {
+        "use strict"
+        //ydot and y could have multiple dimensions, we'll expect each to be an array of length n
+
+        try {
+            var step = [];
+            var yn = ydot(t, y);
+
+            //Step cannot result in a NaN or Infinity
+            Verify.value(yn, "Yn").always().isArray().ofFiniteNumbers();
+
+            for (var i = 0; i < yn.length; i++) {
+                step[i] = y[i] + (yn[i] * dt);
+            }
+            return step;
+        } catch (e) {
+            throw e;
+        }
+    };
+
+
+    return s;
+
+}());
---
+++ src/verifier.js
@@ -0,0 +1,241 @@
+/**
+ * Copyright (c) 2012, Kyle Marcey, Gregory Lee
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Basic JavaScript implementation of Code Contracts, primarily for type checking
+ * Based on code written by CBaxter, and found at http://jstest.codeplex.com/wikipage?title=JavaScript%20Code%20Contract%20Library
+ *
+ * Licensed under the MIT license, which can be found at the link below.
+ * http://jstest.codeplex.com/license
+ */
+
+var Verify = (function () {
+    function hasValue(value) {
+        return typeof (value) !== "undefined" && value !== null;
+    }
+
+    function throwExceptionIf(condition, message, type) {
+        var errorType = type || Error;
+        if (condition) {
+            throw new errorType({message:message});
+        }
+    }
+
+    var _value = null;
+    var _name = null;
+
+    var _defaultVerifier = {
+        type:"DefaultVerifier",
+        getValue:function () {
+            return _value;
+        }
+    };
+    var _arrayVerifier = {
+        type:"ArrayVerifier",
+        getValue:function () {
+            return _value;
+        },
+        withLengthOf:function (length) {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(typeof (length) !== "number", "Specified length must be a 'Number'.", TypeError);
+            throwExceptionIf(hasValue(value) && value.length !== length, "The length of " + name + " must be exactly " + length + ".", RangeError);
+            return _arrayVerifier;
+        },
+        withMinimumLengthOf:function (length) {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(typeof (length) !== "number", "Specified length must be a 'Number'.", TypeError);
+            throwExceptionIf(hasValue(value) && value.length < length, "The length of " + name + " must be at least " + length + ".", RangeError);
+            return _arrayVerifier;
+        },
+        withMaximumLengthOf:function (length) {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(typeof (length) !== "number", "Specified length must be a 'Number'.", TypeError);
+            throwExceptionIf(hasValue(value) && value.length > length, "The length of " + name + " must be less than " + length + ".", RangeError);
+            return _arrayVerifier;
+        },
+        ofFiniteNumbers: function(){
+            var value = _value;
+            var name = _name;
+            value.forEach(function(value, index, array){
+                throwExceptionIf(hasValue(value) && typeof (value) !== "number", "Value at index:" + index.toString() + " must be a number.", TypeError);
+                throwExceptionIf(!isFinite(value), "Value at index: " + index.toString() + " must be a finite number.", TypeError);
+            });
+            return _arrayVerifier;
+        }
+    };
+    var _comparableVerifier = {
+        type:"ComparableVerifier",
+        getValue:function () {
+            return _value;
+        },
+        lessThan:function (val) {
+            var value = _value;
+            var name = _name;
+            var valueSpecified = hasValue(value);
+
+            throwExceptionIf(valueSpecified && typeof (value) !== typeof (val), "Comparison value must be of the same type.", TypeError);
+            throwExceptionIf(valueSpecified && value >= val, name + " must be less than '" + val + "'.", RangeError);
+
+            return _comparableVerifier;
+        },
+        lessThanOrEqualTo:function (val) {
+            var value = _value;
+            var name = _name;
+            var valueSpecified = hasValue(value);
+
+            throwExceptionIf(valueSpecified && typeof (value) !== typeof (val), "Comparison value must be of the same type.", TypeError);
+            throwExceptionIf(valueSpecified && value > val, name + " must be less than or equal to '" + val + "'.", RangeError);
+
+            return _comparableVerifier;
+        },
+        equalTo:function (val) {
+            var value = _value;
+            var name = _name;
+            var valueSpecified = hasValue(value);
+
+            throwExceptionIf(valueSpecified && typeof (value) !== typeof (val), "Comparison value must be of the same type.", TypeError);
+            throwExceptionIf(valueSpecified && value != val, name + " must be equal to '" + val + "'.", RangeError);
+
+            return _comparableVerifier;
+        },
+        notEqualTo:function (val) {
+            var value = _value;
+            var name = _name;
+            var valueSpecified = hasValue(value);
+
+            throwExceptionIf(valueSpecified && typeof (value) !== typeof (val), "Comparison value must be of the same type.", TypeError);
+            throwExceptionIf(valueSpecified && value == val, name + " must not be equal to '" + val + "'.", RangeError);
+
+            return _comparableVerifier;
+        },
+        greaterThanOrEqualTo:function (val) {
+            var value = _value;
+            var name = _name;
+            var valueSpecified = hasValue(value);
+
+            throwExceptionIf(valueSpecified && typeof (value) !== typeof (val), "Comparison value must be of the same type.", TypeError);
+            throwExceptionIf(valueSpecified && value < val, name + " must be greater than or equal to '" + val + "'.", RangeError);
+
+            return _comparableVerifier;
+        },
+        greaterThan:function (val) {
+            var value = _value;
+            var name = _name;
+            var valueSpecified = hasValue(value);
+
+            throwExceptionIf(valueSpecified && typeof (value) !== typeof (val), "Comparison value must be of the same type.", TypeError);
+            throwExceptionIf(valueSpecified && value <= val, name + " must be greater than '" + val + "'.", RangeError);
+
+            return _comparableVerifier;
+        },
+        between:function (lowerBound, upperBound) {
+            var value = _value;
+            var name = _name;
+            var valueSpecified = hasValue(value);
+
+            throwExceptionIf(valueSpecified && typeof (value) !== typeof (lowerBound), "Comparison lowerBound must be of the same type.", TypeError);
+            throwExceptionIf(valueSpecified && typeof (value) !== typeof (upperBound), "Comparison upperBound must be of the same type.", TypeError);
+            throwExceptionIf(valueSpecified && (value < lowerBound || value > upperBound), name + " must be between '" + lowerBound + "' and '" + upperBound + "'.", RangeError);
+
+            return _comparableVerifier;
+        },
+        isFinite: function(){
+            var value = _value;
+            var name = _name;
+
+            throwExceptionIf(!isFinite(value), name + " must be a finite number.", RangeError);
+            return _comparableVerifier;
+        }
+    };
+    var _typeVerifier = {
+        type:"TypeVerifer",
+        getValue:function () {
+            return _value;
+        },
+        isAnything:function () {
+            return _defaultVerifier;
+        },
+        isArray:function () {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(hasValue(value) && !(value instanceof Array), name + " must of type 'Array'.", TypeError);
+            return _arrayVerifier;
+        },
+        isBoolean:function () {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(hasValue(value) && typeof (value) !== "boolean", name + " must be of type 'Boolean'.", TypeError);
+            return _defaultVerifier;
+        },
+        isFunction:function () {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(hasValue(value) && typeof (value) !== "function", name + " must be a function.", TypeError);
+            return _defaultVerifier;
+        },
+        isInstanceOf:function (type) {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(hasValue(value) && !((value) instanceof type), name + " must be of specified type: " + type.toString(), TypeError);
+            return _defaultVerifier;
+        },
+        isNumber:function () {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(hasValue(value) && typeof (value) !== "number", name + " must be of type 'Number'.", TypeError);
+            return _comparableVerifier;
+        },
+        isObject:function () {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(hasValue(value) && typeof (value) !== "object", name + " must be of type 'Object'.", TypeError);
+            return _defaultVerifier;
+        }
+    };
+    var _requiredVerifier = {
+        type:"RequiredVerifier",
+        getValue:function () {
+            return _value;
+        },
+        always:function () {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(typeof (value) === "undefined" || value === null, name + " must not be null or undefined.", ReferenceError);
+            return _typeVerifier;
+        },
+        whenDefined:function () {
+            var value = _value;
+            var name = _name;
+            throwExceptionIf(value === null, name + " must not be null.", ReferenceError);
+            return _typeVerifier;
+        },
+        whenHasValue:function () {
+            return _typeVerifier;
+        },
+        whenNotNull:function () {
+            var type = _type;
+            var name = _name;
+            throwExceptionIf(typeof (value) === "undefined", name + " must not be undefined.", ReferenceError);
+            return _typeVerifier;
+        }
+    };
+
+    return{
+        value:function (value, name) {
+            _value = value;
+            _name = name || "Value";
+            return _requiredVerifier;
+        }
+    };
+})();
